340250a9c555446b33d49cc63768a0907aa4e42c
Jonas Deprund <jonasdeprund@gmail.com>
2021-04-28 15:37:25 +0200

    add styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+// Import styles
+import './styles/app.scss'
+
 // Adding components
 import Player from './components/Player'
 import Song from './components/Song'
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlay } from '@fortawesome/free-solid-svg-icons'
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from '@fortawesome/free-solid-svg-icons'
 
 const Player = () => {
   return (
@@ -11,7 +15,13 @@ const Player = () => {
         <p>End time</p>
       </div>
       <div className="play-control">
-        <FontAwesomeIcon className="play" icon={faPlay} />
+        <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
+        <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+        <FontAwesomeIcon
+          className="skip-forward"
+          size="2x"
+          icon={faAngleRight}
+        />
       </div>
     </div>
   )
