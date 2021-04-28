5920e0d532e08320e35bc899be605f580e525434
Jonas Deprund <jonasdeprund@gmail.com>
2021-04-28 18:09:36 +0200

    added play

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Import styles
 import './styles/app.scss'
@@ -11,10 +11,18 @@ import Song from './components/Song'
 import data from './data'
 
 function App() {
+  // State
+  const [songs, setSongs] = useState(data())
+  const [currentSong, setCurrentSong] = useState(songs[0])
+  const [isPlaying, setIsPlaying] = useState(false)
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
     </div>
   )
 }
