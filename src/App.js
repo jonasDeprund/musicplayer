184d4ae2141663177b0a6d0275aba8fd4f8cfa70
Jonas Deprund <jonasdeprund@gmail.com>
2021-04-29 21:10:52 +0200

    end of the day

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './styles/app.scss'
 // Adding components
 import Player from './components/Player'
 import Song from './components/Song'
+import Library from './components/Library'
 
 // Import data
 import data from './data'
@@ -23,6 +24,7 @@ function App() {
         isPlaying={isPlaying}
         currentSong={currentSong}
       />
+      <Library songs={songs} />
     </div>
   )
 }
---
+++ src/components/Library.js
@@ -0,0 +1,18 @@
+import react from 'react'
+import LibrarySong from './LibrarySong'
+import librarySong from './LibrarySong'
+
+const Library = ({ songs }) => {
+  return (
+    <div className="library">
+      <h2>Library</h2>
+      <div className="library-songs">
+        {songs.map((song) => (
+          <LibrarySong song={song} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Library
